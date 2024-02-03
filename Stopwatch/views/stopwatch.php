1eafad960d171c0f2a8b90b8ee5112e4aeb5a465
Mohamed Hesham <101864682+MohamedBoghdaddy@users.noreply.github.com>
2024-02-03 14:02:36 +0200

    Add files via upload

---
+++ Stopwatch/views/stopwatch.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="styles.css">
+
+</head>
+<body>
+  
+</body>
+<div class="container">
+    <div class = "box form-box">
+       <header>StopWatch</header>
+    <div class="stopwatch">
+         
+          <div class="timer-display">
+          00:00:00:000
+        </div>
+    </div>
+        <div class="buttons">
+            <button id="start-timer">Start</button>
+            <button id="pause-timer">Pause</button>
+            <button id="reset-timer">Reset</button>
+        </div>
+    </div>
+
+</div>         
+
